c108937b0cbfe5a42fb6af60d52fae70fed6a86c
aaronpoweruser <atlrudeboi1@gmail.com>
2013-11-01 14:00:20 -0400

    Fix inheriting props
    Change-Id: I3fc61ad4b8111ef1210ac72645faa9d043833061

--- full_mako.mk
+++ full_mako.mk
@@ -39,4 +39,4 @@ PRODUCT_RESTRICT_VENDOR_FILES := true
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/lge/mako/device.mk)
-$(call inherit-product-if-exists, vendor/lge/mako/device-vendor.mk)
+$(call inherit-product-if-exists, vendor/lge/mako/mako-vendor.mk)
